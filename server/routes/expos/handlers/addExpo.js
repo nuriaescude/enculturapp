b60f40558f8845ae4034e54edfb8efee3bcdab31
Nuria Escude <nur.escude@gmail.com>
2017-06-14 20:20:05 +0200

    changed addExpo

--- server/routes/expos/handlers/addExpo.js
+++ server/routes/expos/handlers/addExpo.js
@@ -5,7 +5,7 @@ function addExpo( req, res ) {
 	expoModelData.category = expoModelData.categories;
 	// { name, center, address, category, url, description, image, price, imageCenter, urlMap, infoCenter, openingToday, openingTimes } 
 	const expo = new Expo(expoModelData)
-	expo.save()
+	Expo.save()
 		.then( () => res.status(200).json({ msg: 'expo inserted properly'}) )
 		.catch( () => res.status(500).json({ msg: 'error inserting expo'}) )
 }
