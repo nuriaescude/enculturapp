a1f7b5c00408862f6b3a78d5f9fb56daf67ab55c
Nuria Escude <nur.escude@gmail.com>
2017-06-16 13:16:26 +0200

    new location path at register screen

--- client/src/js/routes/register/RegisterController.js
+++ client/src/js/routes/register/RegisterController.js
@@ -1,11 +1,14 @@
-function RegisterController($scope, authService) {
+function RegisterController($scope, $location, authService) {
 
 		$scope.register = function(event) {
 			event.preventDefault()
 			const { username, password } = $scope
 
 			authService.register(username, password)
-				.then(console.log)
+				.then(msg => {
+				console.log(msg)
+					$location.path('/login')
+			})
 				.catch(console.log)
 		}
 
